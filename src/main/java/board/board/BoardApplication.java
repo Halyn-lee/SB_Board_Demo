7ea0910132cc59a20017af8210e4909707133ac6
Halyn-Lee <leehalyn@gmail.com>
2023-08-27 02:35:00 +0900

    파일 업로드 관련 설정 추가 1. commons-io, commons-fileupload 의존성 추가 2. multipartResolver 빈 등록 3. BoardApplication 클래스 스프링부트 첨부 파일 자동구성 제거 4. 컨트롤러, 뷰, 서비스 수정 (파일 업로드 및 파일 정보 확인용 임시 로그 처리 등)

--- src/main/java/board/board/BoardApplication.java
+++ src/main/java/board/board/BoardApplication.java
@@ -2,8 +2,10 @@ package board.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
+					// multipartResolver를 등록했기 때문에 스프링부트에서 자동으로 구성된 요소들 중 첨부파일과 관련된 구성 제외
 public class BoardApplication {
 
 	public static void main(String[] args) {
--- src/main/java/board/board/service/BoardServiceImpl.java
+++ src/main/java/board/board/service/BoardServiceImpl.java
@@ -1,14 +1,20 @@
 package board.board.service;
 
+import java.util.Iterator;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import board.board.dto.BoardDto;
 import board.board.mapper.BoardMapper;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Service
+@Slf4j
 public class BoardServiceImpl implements BoardService{
 
     @Autowired
@@ -20,8 +26,24 @@ public class BoardServiceImpl implements BoardService{
     }
 
     @Override
-    public void insertBoard(BoardDto board) throws Exception {
-        boardMapper.insertBoard(board);
+    public void insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
+        //boardMapper.insertBoard(board); 업로드 파일 정보 확인 목적으로 게시글 저장 x
+        if(ObjectUtils.isEmpty(multipartHttpServletRequest) == false){
+            Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
+            String name;
+            while(iterator.hasNext()){
+                name = iterator.next();
+                log.debug("file tag name : " + name);
+                List<MultipartFile> list = multipartHttpServletRequest.getFiles(name);
+                for(MultipartFile multipartFile : list){
+                    log.debug("start file information");
+                    log.debug("file name : " + multipartFile.getOriginalFilename());
+                    log.debug("file size : " + multipartFile.getSize());
+                    log.debug("file content type : " + multipartFile.getContentType());
+                    log.debug("END FILE INFORMATION.\n");
+                }
+            }
+        }
     }
 
     @Override
