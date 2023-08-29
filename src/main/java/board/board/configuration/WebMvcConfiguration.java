de1cd8fbc2060f599d84f4cd915e0ee39d6629ff
Halyn-Lee <leehalyn@gmail.com>
2023-08-30 03:12:51 +0900

    RESTful 게시판으로 변경 (진행) 1. 컨트롤러, 뷰 수정 (restBoard~ 파일 새로 생성 후 작업) 2. HiddenHttpMethodFilter 빈 등록

--- src/main/java/board/board/configuration/WebMvcConfiguration.java
+++ src/main/java/board/board/configuration/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package board.board.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,6 +24,17 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
         commonsMultipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // 5mb
 
         return commonsMultipartResolver;
+    }
+
+    // HTTP Method 에러 처리를 위한 추가 Bean 설정
+    /* Hidden 타입의 input 태그의 속성들을 읽어서
+        HttpServletRequestWrapper.getMethod() 반환 값을 변경해 요청된 HTTP 메소드의 타입을
+        PUT, DELETE, PATCH로 변경해주는 필터 */
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() {
+
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        return hiddenHttpMethodFilter;
     }
 
 }
