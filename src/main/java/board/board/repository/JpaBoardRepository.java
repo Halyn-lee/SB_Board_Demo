a043ccbe6421d50de1e81a48861266ecc9c78c93
Halyn-Lee <leehalyn@gmail.com>
2023-10-15 23:29:45 +0900

    JPA를 사용한 게시판으로 변경 (진행4) - 뷰 생성

--- src/main/java/board/board/repository/JpaBoardRepository.java
+++ src/main/java/board/board/repository/JpaBoardRepository.java
@@ -10,9 +10,11 @@ import board.board.entity.BoardEntity;
 import board.board.entity.BoardFileEntity;
 
 public interface JpaBoardRepository extends CrudRepository<BoardEntity, Integer>{
-
+// 도메인 클래스인 BoardEntity와 id타입인 Integer를 파라미터로 받음
     List<BoardEntity> findAllByOrderByBoardIdxDesc();
+    // 게시글 번호로 정렬하여 전체 게시글을 조회
 
     @Query("SELECT file FROM BoardFileEntity file WHERE board_idx = :boardIdx AND idx = :idx")
+    // 첨부파일 정보 조회
     BoardFileEntity findBoardFile(@Param("boardIdx") int boardIdx, @Param("idx") int idx);
 }
